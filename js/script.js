d5683a461cf5df5b278b1036d02cb90b39cc057b
Felipe Almeida <felipe.lipe12.fl@outlook.com>
2024-05-28 13:30:42 -0300

    Feat: Create archive JS with modal funcion

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-function appsPopup() {
-    
+function appsModal() {
+    let modal = document.getElementById('modal');
+
+    if (modal.classList.contains('hide')) {
+        modal.classList.remove('hide');
+        modal.classList.add('show');
+    } else {
+        modal.classList.remove('show');
+        modal.classList.add('hide');
+    }
 }
